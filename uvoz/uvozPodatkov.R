3ed6c8607e21e5793cdc5bf62c5546b509219aaa
Ana Golob <ana.golob@student.fmf.uni-lj.si>
2017-05-01 20:18:19 +0200

    Update uvozPodatkov.R

--- uvoz/uvozPodatkov.R
+++ uvoz/uvozPodatkov.R
@@ -1,19 +1,4 @@
 # 2. Faza: uvoz podatkov
-library(dplyr)
 
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-
-uvozi.poslovanje <- function() {
-  return(read.csv2("poslovanjePod.csv",
-                   skip = 1,
-                   header = FALSE,
-                   col.names = c("SKD DEJAVNOST", "VRSTA KAZALNIKA", "ÈETRTLETJE", "VREDNOST"),
-                   na=c("", " ")
-                   ))
-}
-
-
-View(tabela1)
 
 
