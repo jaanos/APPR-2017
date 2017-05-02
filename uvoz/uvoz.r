534b1f6ee1f82d0df7ad35ae8b1d2bf5abfd7409
Anže Marinko <amarinko@skavt.net>
2017-05-02 19:02:18 +0200

    uvoz tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,19 +1,149 @@
 # 2. faza: Uvoz podatkov
-
-# .csv datoteke uvežene iz Eurostata
-d <- dir('podatki')
-z <- grep('\\.csv$', d)
-podatki <- d[z]
-
-for (i in 1:length(podatki)) {
-  View(read.csv2(paste('podatki/', podatki[i], sep=''), sep=','))
-  }
+library(readr)
+sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
 
 # .html datoteke uvežene iz Eurostata
 z <- grep('\\.html$', d)
 podatki <- d[z]
 
 html.datoteka <- paste('podatki/', podatki[1], sep='')
+
+# Funkcija, ki uvozi podatke o BDP-jih držav
+uvozi.drzave <- function() {
+  data <- read_csv("podatki/nama_10_pc_1_Data.csv",
+                    locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("leto", 'drzava', 'enota', 'BDP', 'vrednost')
+  return(data)
+}
+View(uvozi.drzave())
+
+# Funkcija, ki uvozi podatke o drzavljanih
+uvozi.drzavljani <- function() {
+  data <- read_csv("podatki/nama_10_pe_1_Data.csv",
+                   locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("leto", 'drzava', 'enota', 'Skupno', 'stevilo')
+  return(data)
+}
+View(uvozi.drzavljani())
+
+# Funkcija, ki uvozi podatke o stevilu mladih
+uvozi.mladi <- function() {
+  data <- read_csv("podatki/yth_demo_010_1_Data.csv",
+                   locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("leto", 'drzava', 'Skupno','enota', 'starost', 'stevilo')
+  return(data)
+}
+View(uvozi.mladi())
+
+# Funkcija, ki uvozi podatke o izobrazenosti mladih
+uvozi.izobrazba <- function() {
+  data <- read_csv("podatki/yth_demo_040_1_Data.csv",
+                   locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("leto", 'drzava', 'izobrazba', 'spol', 'starost', 'enota', 'stevilo')
+  return(data)
+}
+View(uvozi.izobrazba())
+
+# Funkcija, ki uvozi podatke o neformalnem izobrazevanju
+uvozi.neformalno <- function() {
+  data <- read_csv("podatki/yth_educ_060_1_Data.csv",
+                   locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("leto", 'drzava', 'enota', 'spol', 'starost', 'stevilo')
+  return(data)
+}
+View(uvozi.neformalno())
+
+# Funkcija, ki uvozi podatke o zaposlenosti
+uvozi.zaposlenost <- function() {
+  data <- read_csv("podatki/yth_empl_010_1_Data.csv",
+                   locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("leto", 'drzava', 'spol', 'starost', 'enota', 'izobrazba', 'stevilo')
+  return(data)
+}
+View(uvozi.zaposlenost())
+
+# Funkcija, ki uvozi podatke o neaktivnih
+uvozi.neaktivni <- function() {
+  data <- read_csv("podatki/yth_empl_160_1_Data.csv",
+                   locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("leto", 'drzava', 'spol', 'starost', 'delovni status', 'Neaktivnost', 'izobrazba', 'enota', 'vrednost')
+  return(data)
+}
+View(uvozi.neaktivni())
+
+# Funkcija, ki uvozi podatke o vkljucenosti v aktivnosti znotraj religij in cerkva
+uvozi.religija <- function() {
+  data <- read_csv("podatki/yth_part_030_1_Data.csv",
+                   locale = locale(encoding = "Windows-1250"))
+  names(data) <- c("starost", 'drzava', 'enota', 'spol', 'leto', 'DA_NE', 'vrednost')
+  return(data)
+}
+View(uvozi.religija())
+
+# Funkcija, ki uvozi podatke o prostovoljstvu
+uvozi.prostovoljstvo <- function() {
+  stran <- html_session(podatki/yth_volunt_010.html) %>% read_html()
+  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+    .[[1]] %>% html_table(dec = ",")
+  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+                        "ustanovitev", "pokrajina", "regija", "odcepitev")
+  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
+  }
+  for (col in c("obcina", "pokrajina", "regija")) {
+    tabela[[col]] <- factor(tabela[[col]])
+  }
+  return(tabela)
+}
+
+# Funkcija, ki uvozi podatke iz html
+uvozi.obcine <- function() {
+  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+  stran <- html_session(podatki/yth_volunt_010.html) %>% read_html()
+  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+    .[[1]] %>% html_table(dec = ",")
+  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+                        "ustanovitev", "pokrajina", "regija", "odcepitev")
+  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
+  }
+  for (col in c("obcina", "pokrajina", "regija")) {
+    tabela[[col]] <- factor(tabela[[col]])
+  }
+  return(tabela)
+}
+
+# Funkcija, ki uvozi podatke iz datoteke druzine.csv
+uvozi.druzine <- function(obcine) {
+  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
+                    locale = locale(encoding = "Windows-1250"))
+  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
+    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
+  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
+  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
+                        value.name = "stevilo.druzin")
+  data$velikost.druzine <- parse_number(data$velikost.druzine)
+  data$obcina <- factor(data$obcina, levels = obcine)
+  return(data)
+}
+
+# Zapišimo podatke v razpredelnico obcine
+obcine <- uvozi.obcine()
+
+# Zapišimo podatke v razpredelnico druzine.
+druzine <- uvozi.druzine(levels(obcine$obcina))
+
+# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
+# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
+# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
+# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
+# fazah.
 
 # Funkcija, ki uvozi občine iz Wikipedije
 uvozi.obcine <- function() {
