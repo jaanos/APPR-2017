a3f61f6c8af3ed0829c66478e284e1dce2fda691
Anže Marinko <amarinko@skavt.net>
2017-05-04 00:10:16 +0200

    sprememba uvoza

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,84 +1,117 @@
 # 2. faza: Uvoz podatkov
 library(readr)
-sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
+library(dplyr)
 
-# .html datoteke uvežene iz Eurostata
-z <- grep('\\.html$', d)
-podatki <- d[z]
-
-html.datoteka <- paste('podatki/', podatki[1], sep='')
-
-# Funkcija, ki uvozi podatke o BDP-jih držav
+# Funkcija, ki uvozi podatke o BDP-jih držav (v EUR na osebo)
 uvozi.drzave <- function() {
   data <- read_csv("podatki/nama_10_pc_1_Data.csv",
-                    locale = locale(encoding = "Windows-1250"))
+                   locale = locale(encoding = "Windows-1250"), na=':')
   names(data) <- c("leto", 'drzava', 'enota', 'BDP', 'vrednost')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  izbris2 <- data$enota == data$enota[grep("Current prices, euro per capita", data$enota, ignore.case=TRUE)]
+  data <- data[izbris2,]
+  izbris3 <- data$BDP == data$BDP[grep("Gross domestic product at market prices", data$BDP, ignore.case=TRUE)]
+  data <- data[izbris3,]
+  data$enota <- NULL
+  data$BDP <- NULL
   return(data)
 }
-View(uvozi.drzave())
 
-# Funkcija, ki uvozi podatke o drzavljanih
+# Funkcija, ki uvozi podatke o drzavljanih (v tisoč)
 uvozi.drzavljani <- function() {
   data <- read_csv("podatki/nama_10_pe_1_Data.csv",
-                   locale = locale(encoding = "Windows-1250"))
+                   locale = locale(encoding = "Windows-1250"), na=':')
   names(data) <- c("leto", 'drzava', 'enota', 'Skupno', 'stevilo')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  izbris2 <- data$enota == data$enota[grep("Thousand persons", data$enota, ignore.case=TRUE)]
+  data <- data[izbris2,]
+  data$enota <- NULL
+  data$Skupno <- NULL
   return(data)
 }
-View(uvozi.drzavljani())
 
-# Funkcija, ki uvozi podatke o stevilu mladih
+# Funkcija, ki uvozi podatke o stevilu mladih (stevilo)
 uvozi.mladi <- function() {
   data <- read_csv("podatki/yth_demo_010_1_Data.csv",
-                   locale = locale(encoding = "Windows-1250"))
+                   locale = locale(encoding = "Windows-1250"), na=':')
   names(data) <- c("leto", 'drzava', 'Skupno','enota', 'starost', 'stevilo')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  data$Skupno <- NULL
+  data$enota <- NULL
   return(data)
 }
-View(uvozi.mladi())
 
-# Funkcija, ki uvozi podatke o izobrazenosti mladih
+# Funkcija, ki uvozi podatke o izobrazenosti mladih (v odstotkih)
 uvozi.izobrazba <- function() {
   data <- read_csv("podatki/yth_demo_040_1_Data.csv",
-                   locale = locale(encoding = "Windows-1250"))
-  names(data) <- c("leto", 'drzava', 'izobrazba', 'spol', 'starost', 'enota', 'stevilo')
+                   locale = locale(encoding = "Windows-1250"), na=':')
+  names(data) <- c("leto", 'drzava', 'izobrazba', 'spol', 'starost', 'enota', 'odstotek')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  data$enota <- NULL
   return(data)
 }
-View(uvozi.izobrazba())
 
-# Funkcija, ki uvozi podatke o neformalnem izobrazevanju
+# Funkcija, ki uvozi podatke o neformalnem izobrazevanju (v odstotkih)
 uvozi.neformalno <- function() {
   data <- read_csv("podatki/yth_educ_060_1_Data.csv",
-                   locale = locale(encoding = "Windows-1250"))
-  names(data) <- c("leto", 'drzava', 'enota', 'spol', 'starost', 'stevilo')
+                   locale = locale(encoding = "Windows-1250"), na=':')
+  names(data) <- c("leto", 'drzava', 'enota', 'spol', 'starost', 'odstotek')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  data$enota <- NULL
   return(data)
 }
-View(uvozi.neformalno())
 
-# Funkcija, ki uvozi podatke o zaposlenosti
+# Funkcija, ki uvozi podatke o zaposlenosti (v odstotkih)
 uvozi.zaposlenost <- function() {
   data <- read_csv("podatki/yth_empl_010_1_Data.csv",
-                   locale = locale(encoding = "Windows-1250"))
-  names(data) <- c("leto", 'drzava', 'spol', 'starost', 'enota', 'izobrazba', 'stevilo')
+                   locale = locale(encoding = "Windows-1250"), skip=1, na=':')
+  names(data) <- c("leto", 'drzava', 'spol', 'starost', 'enota', 'izobrazba', 'odstotek')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  izbris2 <- data$enota != data$enota[grep("Percentage", data$enota, ignore.case=TRUE)]
+  data <- data[izbris2,]
+  data$enota <- NULL
   return(data)
 }
-View(uvozi.zaposlenost())
 
-# Funkcija, ki uvozi podatke o neaktivnih
+# Funkcija, ki uvozi podatke o neaktivnih (nezaposlenih, nešolajočih se) (v odstotkih)
 uvozi.neaktivni <- function() {
   data <- read_csv("podatki/yth_empl_160_1_Data.csv",
-                   locale = locale(encoding = "Windows-1250"))
-  names(data) <- c("leto", 'drzava', 'spol', 'starost', 'delovni status', 'Neaktivnost', 'izobrazba', 'enota', 'vrednost')
+                   locale = locale(encoding = "Windows-1250"), skip=1, na=':')
+  names(data) <- c("leto", 'drzava', 'spol', 'starost', 'zaposlen', 'Neaktivnost', 'izobrazba', 'enota', 'odstotek')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  data$enota <- NULL
+  data$zaposlen <- NULL
+  data$Neaktivnost <- NULL
+  data$izobrazba <- NULL
   return(data)
 }
-View(uvozi.neaktivni())
 
-# Funkcija, ki uvozi podatke o vkljucenosti v aktivnosti znotraj religij in cerkva
+# Funkcija, ki uvozi podatke o vkljucenosti v aktivnosti znotraj religij in cerkva (v odstotkih)
 uvozi.religija <- function() {
   data <- read_csv("podatki/yth_part_030_1_Data.csv",
-                   locale = locale(encoding = "Windows-1250"))
-  names(data) <- c("starost", 'drzava', 'enota', 'spol', 'leto', 'DA_NE', 'vrednost')
+                   locale = locale(encoding = "Windows-1250"), na=':')
+  names(data) <- c("starost", 'drzava', 'enota', 'spol', 'leto', 'DA_NE', 'odstotek')
+  izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
+  data <- data[!izbris,]
+  data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  data$enota <- NULL
+  data$DA_NE <- NULL
   return(data)
 }
-View(uvozi.religija())
 
 # Funkcija, ki uvozi podatke o prostovoljstvu
 uvozi.prostovoljstvo <- function() {
@@ -99,100 +132,12 @@ uvozi.prostovoljstvo <- function() {
   return(tabela)
 }
 
-# Funkcija, ki uvozi podatke iz html
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(podatki/yth_volunt_010.html) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec = ",")
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-                    locale = locale(encoding = "Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-                        value.name = "stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels = obcine)
-  return(data)
-}
-
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
-
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
-
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec = ",")
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in colnames(tabela)) {
-    tabela[tabela[[col]] == "-", col] <- NA
-  }
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.numeric(tabela[[col]])) {
-      next()
-    }
-    tabela[[col]] <- gsub("[.*]", "", tabela[[col]]) %>% as.numeric()
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-                    locale = locale(encoding = "Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-                        value.name = "stevilo.druzin")
-  data$velikost.druzine <- as.numeric(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels = obcine)
-  return(data)
-}
-
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
-
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+drzave <- uvozi.drzave()
+drzavljani <- uvozi.drzavljani()
+mladi <- uvozi.mladi()
+izobrazba <- uvozi.izobrazba()
+neformalno <- uvozi.neformalno()
+zaposlenost <- uvozi.zaposlenost()
+neaktivni <- uvozi.neaktivni()
+religija <- uvozi.religija()
+prostovoljstvo <- uvozi.prostovoljstvo()
