77b1920efa2c7642dcc4e30584ca2f8310d1a510
Anže <amarinko@skavt.net>
2017-05-15 18:54:04 +0200

    risanje grafa za BDP

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,19 @@
 # 3. faza: Vizualizacija podatkov
-
+library(ggplot2)
+library(dplyr)
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
                              "OB/OB", encoding = "Windows-1250")
 levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
   { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
+zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(drzave$drzava))
 zemljevid <- pretvori.zemljevid(zemljevid)
 
 # Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+#povprecja <- druzine %>% group_by(obcina) %>%
+#  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+
+g <- ggplot(drzave) + aes(x=leto, y=vrednost, color = drzava) +
+  geom_line() + ggtitle("BDP")
+# ggplot(drzave %>% filter(drzava == 'Slovenia'))
+print(g)
