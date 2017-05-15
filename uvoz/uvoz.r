f124a940be97cbcd23b0451e23b54bd90f4a0cdc
Anže <amarinko@skavt.net>
2017-05-15 18:12:09 +0200

    urejanje tabel

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,7 +1,9 @@
 # 2. faza: Uvoz podatkov
+# drzave, drzavljani, mladi, izobrazba, neformalno, zaposlenost, neaktivni, religija, prostovoljstvo
+library(rvest)
+library(gsubfn)
 library(readr)
 library(dplyr)
-library(httr)
 
 # Funkcija, ki uvozi podatke o BDP-jih držav (v EUR na osebo)
 uvozi.drzave <- function() {
@@ -22,7 +24,6 @@ uvozi.drzave <- function() {
 drzave <- uvozi.drzave()
 drzave$vrednost <- parse_number(drzave$vrednost, na=c(NA, ":"))
 drzave <- na.omit(drzave)
-View(drzave)
 
 # Funkcija, ki uvozi podatke o drzavljanih (v tisoč)
 uvozi.drzavljani <- function() {
@@ -41,7 +42,6 @@ uvozi.drzavljani <- function() {
 drzavljani <- uvozi.drzavljani()
 drzavljani$stevilo <- parse_number(drzavljani$stevilo, na=c(NA, ":"))
 drzavljani <- na.omit(drzavljani)
-View(drzavljani)
 
 # Funkcija, ki uvozi podatke o stevilu mladih (stevilo)
 uvozi.mladi <- function() {
@@ -51,6 +51,8 @@ uvozi.mladi <- function() {
   izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
   data <- data[!izbris,]
   data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  data$starost <- gsub("From 15 to 29 years", "15 do 29", data$starost, ignore.case=TRUE)
+  data$starost <- gsub("Less than 15 years", "do 15", data$starost, ignore.case=TRUE)
   data$Skupno <- NULL
   data$enota <- NULL
   data$komentarji <- NULL
@@ -59,9 +61,8 @@ uvozi.mladi <- function() {
 mladi <- uvozi.mladi()
 mladi$stevilo <- parse_number(mladi$stevilo, na=c(NA, ":"))
 mladi <- na.omit(mladi)
-View(mladi)
 
-# Funkcija, ki uvozi podatke o izobrazenosti mladih (v odstotkih)
+# Funkcija, ki uvozi podatke o izobrazenosti mladih (v odstotkih, ženske)
 uvozi.izobrazba <- function() {
   data <- read_csv("podatki/yth_demo_040_1_Data.csv",
                    locale = locale(encoding = "Windows-1250"), na=':')
@@ -70,15 +71,17 @@ uvozi.izobrazba <- function() {
   data <- data[!izbris,]
   data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
   data$enota <- NULL
+  data$spol <- NULL
+  data$starost <- NULL
+  data$izobrazba <- NULL
   data$komentarji <- NULL
   return(data)
 }
 izobrazba <- uvozi.izobrazba()
 izobrazba$stevilo <- parse_number(izobrazba$stevilo, na=c(NA, ":"))
 izobrazba <- na.omit(izobrazba)
-View(izobrazba)
 
-# Funkcija, ki uvozi podatke o neformalnem izobrazevanju (v odstotkih)
+# Funkcija, ki uvozi podatke o neformalnem izobrazevanju (v odstotkih, stari 15-24)
 uvozi.neformalno <- function() {
   data <- read_csv("podatki/yth_educ_060_1_Data.csv",
                    locale = locale(encoding = "Windows-1250"), na=':')
@@ -86,14 +89,16 @@ uvozi.neformalno <- function() {
   izbris <- data$drzava == data$drzava[grep("^Euro", data$drzava, ignore.case=TRUE)]
   data <- data[!izbris,]
   data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
+  izbris2 <- data$spol != data$spol[grep("Total", data$spol, ignore.case=TRUE)]
+  data <- data[izbris2,]
   data$enota <- NULL
   data$komentarji <- NULL
+  data$starost <- NULL
   return(data)
 }
 neformalno <- uvozi.neformalno()
 neformalno$stevilo <- parse_number(neformalno$stevilo, na=c(NA, ":"))
 neformalno <- na.omit(neformalno)
-View(neformalno)
 
 # Funkcija, ki uvozi podatke o zaposlenosti (v odstotkih)
 uvozi.zaposlenost <- function() {
@@ -105,6 +110,12 @@ uvozi.zaposlenost <- function() {
   data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
   izbris2 <- data$enota != data$enota[grep("Percentage", data$enota, ignore.case=TRUE)]
   data <- data[izbris2,]
+  izbris3 <- data$spol != data$spol[grep("Total", data$spol, ignore.case=TRUE)]
+  data <- data[izbris3,]
+  data$starost <- gsub("[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*", "\\1 do \\2", data$starost, ignore.case=TRUE)
+  izbris4 <- data$izobrazba != data$izobrazba[grep("^((All)|(No))", data$izobrazba, ignore.case=TRUE)]
+  data <- data[izbris4,]
+  data$izobrazba <- gsub(".+\\(levels ([0-9])[^0-9]+([0-9])\\)$", "\\1-\\2", data$izobrazba, ignore.case=TRUE)
   data$enota <- NULL
   data$komentarji <- NULL
   return(data)
@@ -112,9 +123,8 @@ uvozi.zaposlenost <- function() {
 zaposlenost <- uvozi.zaposlenost()
 zaposlenost$stevilo <- parse_number(zaposlenost$stevilo, na=c(NA, ":"))
 zaposlenost <- na.omit(zaposlenost)
-View(zaposlenost)
 
-# Funkcija, ki uvozi podatke o neaktivnih (nezaposlenih, nešolajočih se) (v odstotkih)
+# Funkcija, ki uvozi podatke o neaktivnih (nezaposlenih, nešolajočih se, starih 15-19) (v odstotkih)
 uvozi.neaktivni <- function() {
   data <- read_csv("podatki/yth_empl_160_1_Data.csv",
                    locale = locale(encoding = "Windows-1250"), skip=1, na=':')
@@ -123,6 +133,7 @@ uvozi.neaktivni <- function() {
   data <- data[!izbris,]
   data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
   data$enota <- NULL
+  data$starost <- NULL
   data$komentarji <- NULL
   data$zaposlen <- NULL
   data$spol <- NULL
@@ -133,9 +144,8 @@ uvozi.neaktivni <- function() {
 neaktivni <- uvozi.neaktivni()
 neaktivni$vrednost <- parse_number(neaktivni$vrednost, na=c(NA, ":"))
 neaktivni <- na.omit(neaktivni)
-View(neaktivni)
 
-# Funkcija, ki uvozi podatke o vkljucenosti v aktivnosti znotraj religij in cerkva (v odstotkih)
+# Funkcija, ki uvozi podatke o vkljucenosti v aktivnosti znotraj religij in cerkva (v odstotkih, 2006)
 uvozi.religija <- function() {
   data <- read_csv("podatki/yth_part_030_1_Data.csv",
                    locale = locale(encoding = "Windows-1250"), na=':')
@@ -145,32 +155,50 @@ uvozi.religija <- function() {
   data$drzava <- gsub(" \\(.*\\)$", "", data$drzava, ignore.case=TRUE)
   izbris2 <- data$spol != data$spol[grep("Total", data$spol, ignore.case=TRUE)]
   data <- data[izbris2,]
+  data$starost <- gsub("[^0-9]*([0-9]+)[^0-9]*([0-9]+)[^0-9]*", "od\\1do\\2", data$starost, ignore.case=TRUE)
   data$enota <- NULL
   data$DA_NE <- NULL
+  data$leto <- NULL
   return(data)
 }
 religija <- uvozi.religija()
 religija$vrednost <- parse_number(religija$vrednost, na=c(NA, ":"))
 religija <- na.omit(religija)
-View(religija)
 
-# Funkcija, ki uvozi podatke o prostovoljstvu
+# Funkcija, ki uvozi podatke o prostovoljstvu (v odstotkih za 2006)
 uvozi.prostovoljstvo <- function() {
-  stran <- GET('http://appsso.eurostat.ec.europa.eu/nui/download?p=92c60fbc-4bf4-47ef-9757-17147c788370-1494233436416_&_=1494233696613') %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec = ",")
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
+  stran <- 'podatki/yth_volunt_010.html' %>% read_html()
+  moski <- stran %>% html_nodes(xpath="//table") %>%
+    .[[2]] %>% html_table(dec = ".")
+  colnames(moski) <- c("drzava", "od16do29", "od20do29", "od16do19", "od16do24",
+                        "od20do24", "od25do29")
+  izbris <- moski$drzava == moski$drzava[grep("^Euro", moski$drzava, ignore.case=TRUE)]
+  moski <- moski[!izbris,]
+  moski <- moski[-28,]
+  moski$drzava <- gsub(" \\(.*\\)$", "", moski$drzava, ignore.case=TRUE)
+  moski$spol <- 'males'
+  zenske <- stran %>% html_nodes(xpath="//table") %>%
+    .[[3]] %>% html_table(dec = ".")
+  colnames(zenske) <- c("drzava", "od16do29", "od20do29", "od16do19", "od16do24",
+                        "od20do24", "od25do29")
+  izbris <- zenske$drzava == zenske$drzava[grep("^Euro", zenske$drzava, ignore.case=TRUE)]
+  zenske <- zenske[!izbris,]
+  zenske <- zenske[-28,]
+  zenske$drzava <- gsub(" \\(.*\\)$", "", zenske$drzava, ignore.case=TRUE)
+  zenske$spol <- 'females'
+  moski$od16do29 <- NULL
+  moski$od20do29 <- NULL
+  moski$od16do24 <- NULL
+  zenske$od16do29 <- NULL
+  zenske$od20do29 <- NULL
+  zenske$od16do24 <- NULL
+  tabela <- rbind(moski, zenske)
+  for (col in c("od16do19", "od20do24", "od25do29")) {
+    tabela[[col]] <- parse_number(tabela[[col]], na = "-")
   }
-  for (col in c("obcina", "pokrajina", "regija")) {
+  for (col in c("drzava", "spol")) {
     tabela[[col]] <- factor(tabela[[col]])
   }
   return(tabela)
 }
-
 prostovoljstvo <- uvozi.prostovoljstvo()
