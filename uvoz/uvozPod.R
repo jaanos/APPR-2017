5012661ac982b309cd1b9721063eb0c243b4d691
Ana Golob <ana.golob19@gmail.com>
2017-05-18 21:58:19 +0200

    Dokončana koda za uvoz csv.

--- uvoz/uvozPod.R
+++ uvoz/uvozPod.R
@@ -1,32 +1,52 @@
 # 2. Faza: uvoz podatkov
 library(dplyr)
+library(reader)
+library(stats)
+library(tidyr)
 
+this.dir <- dirname(parent.frame(2)$ofile) # frame(3) also works.
+setwd(this.dir)
+setwd('..')
+trenutno = setwd(Sys.getenv("HOME"));
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
 
-uvozi.meritve <- function() {
-  return(read.csv2("meritve.csv",
-                   skip = 1,
-                   header = FALSE,
-                   col.names = c("OBÈINA", "VRSTA MERITVE", "LETO", "VREDNOST"),
-                   na=c("", " "),
-                   ))
+uvozi.meritve <- function(trenutno) {
+  fpot = file.path(trenutno, 'podatki', "meritve.csv");
+  tabela <- read.csv2(fpot,
+                      skip = 1,
+                      header = FALSE,
+                      col.names = c("OBÈINA", "VRSTA MERITVE", "LETO", "VREDNOST"),
+                      na=c("", " ")
+  )
+  tabela <- tabela %>% fill(1:2)
+  vrstice.z.na <- apply(tabela, 1, function(x){any(is.na(x))})
+  tabela <- tabela[!vrstice.z.na,]
+  return(tabela)
 }
 
-tabela1 <- uvozi.meritve()
+
+tabela1 <- uvozi.meritve(trenutno)
 View(tabela1)
 
-uvozi.place <- function() {
-  return(read.csv2("place.csv",
-                   skip = 3,
-                   header = FALSE,
-                   col.names = c("REGIJA","LETO","POVPREÈNA MESEÈNA BRUTO PLAÈA"),
-                   na=c("", " ")
-  ))
+uvozi.place <- function(trenutno) {
+  fpot = file.path(trenutno, 'podatki', "place.csv");
+  tabela <- read.csv2(fpot,
+                      col.names = c("OBÈINA","LETO","POVPREÈNA MESEÈNA BRUTO PLAÈA"),
+                      skip = 3,
+                      header = FALSE,
+                      na=c("", " ")
+  )
+  tabela <- tabela %>% fill(1) 
+  vrstice.z.na <- apply(tabela, 1, function(x){any(is.na(x))})
+  tabela <- tabela[!vrstice.z.na,]
+  return(tabela)
 }
 
-tabela2 <- uvozi.place()
+tabela2 <- uvozi.place(trenutno)
 View(tabela2)
+
+
 
 
 
