d0d6ef1b15f741cd11a32c806e7b7d0061044bc3
Ana Golob <ana.golob@student.fmf.uni-lj.si>
2017-05-22 11:05:04 +0200

    Uvoz zemljevida

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,12 +1,84 @@
 # 2. faza: Uvoz podatkov:
 
 # Knjižnjice:
+Encoding("UTF-8")
 library(dplyr)
 library(rvest)
-Encoding("Windows-1250")
+library(dplyr)
+library(reader)
+library(stats)
+library(tidyr)
+
+# Nastavitev relativne poti do csv datotek:
+
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+setwd('..')
+trenutno = setwd(Sys.getenv("HOME"));
+
+
+# Funkciji, ki uvozita podatke iz datoteke druzine csv:
+
+uvozi.meritve <- function(trenutno) {
+  fpot = file.path(trenutno, 'podatki', "meritve.csv");
+  tabela <- read.csv2(fpot,
+                      skip = 1,
+                      header = FALSE,
+                      col.names = c("OB?INA", "VRSTA MERITVE", "LETO", "VREDNOST"),
+                      na=c("", " ")
+  )
+  tabela <- tabela %>% fill(1:2)
+  vrstice.z.na <- apply(tabela, 1, function(x){any(is.na(x))})
+  tabela <- tabela[!vrstice.z.na,]
+  return(tabela)
+}
+
+uvozi.place <- function(trenutno) {
+  fpot = file.path(trenutno, 'podatki', "place.csv");
+  tabela <- read.csv2(fpot,
+                      col.names = c("OB?INA","LETO","POVPRE?NA MESE?NA BRUTO PLA?A"),
+                      skip = 3,
+                      header = FALSE,
+                      na=c("", " ")
+  )
+  tabela <- tabela %>% fill(1) 
+  vrstice.z.na <- apply(tabela, 1, function(x){any(is.na(x))})
+  tabela <- tabela[!vrstice.z.na,]
+  return(tabela)
+}
+
+
+uvozi.turizem <- function(trenutno) {
+  fpot = file.path(trenutno, 'podatki', "turizem.csv");
+  tabela <- read.csv2(fpot,
+                      skip = 4,
+                      na=c("", " "),
+                      col.names = c("OB?INA", "LETO", "PARAMETER" , "VREDNOST"),
+                      header = FALSE
+  )
+  tabela <- tabela %>% fill(1:2)
+  vrstice.z.na <- apply(tabela, 1, function(x){any(is.na(x))})
+  tabela <- tabela[!vrstice.z.na,]
+  return(tabela)  
+}
+
+
+# Klicanje zgornjih funkcij:
+
+tabela1 <- uvozi.meritve(trenutno)
+#View(tabela1)
+
+tabela2 <- uvozi.place(trenutno)
+#View(tabela2)
+
+tabela3 <- uvozi.turizem(trenutno)
+#View(tabela3)
+
+
 
 # Funkcija, ki uvozi podatke iz Wikipedije:
 uvozi.obcine <- function() {
+  encoding = "Windows-1250"
   link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
   stran <- html_session(link) %>% read_html()
   tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
@@ -34,5 +106,5 @@ uvozi.obcine <- function() {
 
 # Klicanje funkcije:
 obcine <- uvozi.obcine()
-View(obcine)
+#View(obcine)
 
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,12 @@
 # 3. faza: Vizualizacija podatkov
+#source("uvoz.r")
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
 
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
-zemljevid <- pretvori.zemljevid(zemljevid)
+Svet <- map_data("world")
+head(Svet)
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+slo <- uv
