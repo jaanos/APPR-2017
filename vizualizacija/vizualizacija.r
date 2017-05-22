3514df250e1f691c919a1ae2c394b7f15478012d
Ana Golob <ana.golob@student.fmf.uni-lj.si>
2017-05-22 11:07:21 +0200

    Uvoz zemljevida

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -6,7 +6,6 @@ library(maps)
 library(mapdata)
 
 # Uvozimo zemljevid.
-Svet <- map_data("world")
-head(Svet)
 
-slo <- uv
+slo <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip", "SVN_adm1") %>% pretvori.zemljevid()
+ggplot() + geom_polygon(data = slo, aes(x = long, y = lat, group = group), color = "black")
